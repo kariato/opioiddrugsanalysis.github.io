103c21ce62939c78d3d196fb33142e23cc88dc48
Mark Davey <mdavey8@gatech.edu>
2021-04-21 01:49:12 -0500

    Pushed A;;

---
+++ src/assets/js/demo_1/dashboard (2).js
@@ -0,0 +1,30 @@
+(function ($) {
+  'use strict';
+  $(function () {
+    var lineStatsOptions = {
+      scales: {
+        yAxes: [{
+          display: false
+        }],
+        xAxes: [{
+          display: false
+        }]
+      },
+      legend: {
+        display: false
+      },
+      elements: {
+        point: {
+          radius: 0
+        },
+        line: {
+          tension: 0
+        }
+      },
+      stepsize: 100
+      }
+      console.log(drugTotals);
+
+  });
+
+})(jQuery);
